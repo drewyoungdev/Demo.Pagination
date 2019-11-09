565ee0555e3dc9aa222debd3967a064a72c71538
Andrew Young <young.andrew2@outlook.com>
2019-11-09 14:44:00 -0500

    Add spark design systems

--- PaginationWeb-Angular/src/app/app.module.ts
+++ PaginationWeb-Angular/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { SparkAngularModule } from "@sparkdesignsystem/spark-angular";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+
+const appRoutes: Routes = [];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SparkAngularModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
